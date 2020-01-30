b5768ae4c1d93bf131476083bba9e006127c8f95
unknown <flemale1@uw.edu>
2020-01-30 14:31:59 -0800

    Finished ch11 ex2

--- chapter-11-exercises/exercise-2/exercise.R
+++ chapter-11-exercises/exercise-2/exercise.R
@@ -8,41 +8,55 @@
 library(fueleconomy)
 
 # Install and load the "dplyr" library
+library(dplyr)
 
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
+makes_vector <- select(vehicles, make)
 
 
 # Use the `distinct()` function to determine how many different car manufacturers
 # are represented by the data set
+distinct(makes_vector)
 
 
 # Filter the data set for vehicles manufactured in 1997
+vehicles_1997 <- filter(vehicles, year == 1997)
 
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
+vehicles_1997 <- arrange(vehicles_1997, hwy)
 
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
+mutate(vehicles_1997, average = (cty + hwy)/2)
 
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
+vehicles_2wd <- filter(vehicles_1997, drive == "Rear-Wheel Drive" | drive == "Front-Wheel Drive", cty > 20)
 
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
+print(select(filter(vehicles_2wd, hwy == min(hwy)), id))
 
 
 # Write a function that takes a `year_choice` and a `make_choice` as parameters,
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
 # of that make in that year.
 # You'll need to filter more (and do some selecting)!
-
+efficient_choice <- function(year_choice, make_choice){
+  vehicles_filtered <- filter(vehicles, year == year_choice, make == make_choice)
+  return(select(
+    (filter(vehicles_filtered, hwy == max(hwy))
+     ), model))
+}
 
 # What was the most efficient Honda model of 1995?
+efficient_choice(1995, "Honda")
 
